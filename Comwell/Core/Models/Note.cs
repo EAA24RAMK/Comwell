6d66623f8f85716ad71a28d1c1a516214fd8737c
Rasmus Møller Kristensen <eaa24ramk@students.eaaa.dk>
2025-05-09 11:39:50 +0200

    lille rettelse

--- Comwell/Core/Models/Note.cs
+++ Comwell/Core/Models/Note.cs
@@ -2,7 +2,7 @@ namespace Core.Models;
 
 public class Note
 {
-    public string Id { get; set; } = "";
+    public int Id { get; set; }
     public string Text { get; set; } = "";
     public DateTime CreatedAt { get; set; }
 }
