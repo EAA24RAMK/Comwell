56edfd7b1410867cc8a354bcd86dd086d9b2de4f
Kaspartacuzz <eaa24kassj@students.eaaa.dk>
2025-05-14 10:29:57 +0200

    opdateret opret-plan

--- Comwell/Core/Models/StudentPlan.cs
+++ Comwell/Core/Models/StudentPlan.cs
@@ -3,7 +3,6 @@ namespace Core.Models;
 public class StudentPlan
 {
     public int Id { get; set; }
-    public string Title { get; set; } = "";
     public int StudentId { get; set; } 
     public int TemplateId { get; set; }
     public string CreatedBy { get; set; } = "";
