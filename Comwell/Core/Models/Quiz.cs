83ea9541e642fd1375002931f6d88cd69ae7bc42
C-W-McCall <elias@liebman.dk>
2025-05-16 09:26:53 +0200

    Slettet LearningResource

--- Comwell/Core/Models/Quiz.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Core.Models;
-
-// Denne klasse beskriver en quiz, som indlejres i LearningResource
-public class Quiz
-{
-    public string Question { get; set; }
-    public List<string> Options { get; set; }
-    public int CorrectAnswerIndex { get; set; }
-}
