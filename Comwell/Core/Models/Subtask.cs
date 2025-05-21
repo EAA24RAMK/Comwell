07edc80f407db1c52af02c2762e9bb5838bf6183
Kaspartacuzz <eaa24kassj@students.eaaa.dk>
2025-05-21 11:47:43 +0200

    I gang

--- Comwell/Core/Models/Subtask.cs
+++ Comwell/Core/Models/Subtask.cs
@@ -3,6 +3,6 @@ namespace Core.Models;
 public class Subtask
 {
     public string Text { get; set; } = "";
-    public bool IsCompleted { get; set; }
+    public string Status { get; set; } = "Ikke startet";
     public bool IsRequestedCompleted { get; set; } 
 }
