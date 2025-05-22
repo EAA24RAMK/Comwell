6e1696d8c4fd48264de5bb24f507f430c38117d0
Kaspartacuzz <eaa24kassj@students.eaaa.dk>
2025-05-22 11:11:26 +0200

    elevpaner skoleperiode

--- Comwell/Core/Models/SchoolPeriod.cs
+++ Comwell/Core/Models/SchoolPeriod.cs
@@ -7,6 +7,8 @@ public class SchoolPeriod
     public int? TemplateId { get; set; }
     public string Status { get; set; } = "Ikke startet";
     public DateTime? StartDate { get; set; }
+    public bool IsDateConfirmed { get; set; }
     public int DurationWeeks { get; set; }
     public bool IsApproved { get; set; } = false;
+    
 }
