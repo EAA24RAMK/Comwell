77446a323268c6745816b3e76544e262745fc7c3
Rasmus Møller Kristensen <eaa24ramk@students.eaaa.dk>
2025-05-25 12:58:00 +0200

    kommentar

--- Comwell/WebApp/Program.cs
+++ Comwell/WebApp/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddScoped<INotificationService, NotificationService>();
 builder.Services.AddScoped<ILearningMaterialService, LearningMaterialService>();
 
 builder.Services.AddScoped(sp =>
-    new HttpClient { BaseAddress = new Uri("https://comwelltpapi.azurewebsites.net") }
+    new HttpClient { BaseAddress = new Uri("https://comwelltpapi.azurewebsites.net") } // ServerAPI URL
 );
 
 await builder.Build().RunAsync();
