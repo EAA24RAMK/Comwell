6109fc52cf6d1cb67b593469a1e69537c196a07c
Rasmus Møller Kristensen <eaa24ramk@students.eaaa.dk>
2025-05-29 14:18:04 +0200

    etas

--- Comwell/WebApp/Services/User/UserService.cs
+++ Comwell/WebApp/Services/User/UserService.cs
@@ -1,24 +1,28 @@
 using Core.Models;
 using System.Net.Http.Json;
-using System.Text.Json;
-using Microsoft.JSInterop;
+using Blazored.LocalStorage;
 
 namespace WebApp.Services;
 
 public class UserService : IUserService
 {
     private readonly HttpClient _http;
-    private readonly IJSRuntime _js;
+    private readonly ILocalStorageService _localStorage;
 
-    public UserService(HttpClient http, IJSRuntime js)
+    public UserService(HttpClient http, ILocalStorageService localStorage)
     {
         _http = http;
-        _js = js;
+        _localStorage = localStorage;
     }
 
     public async Task<List<User>> GetAllUsersAsync()
     {
         return await _http.GetFromJsonAsync<List<User>>("api/user") ?? new();
+    }
+
+    public async Task<User?> GetUserByEmailAsync(string email)
+    {
+        return await _http.GetFromJsonAsync<User>($"api/user/email/{email}");
     }
 
     public async Task<User?> CreateUserAsync(User newUser)
@@ -28,18 +32,20 @@ public class UserService : IUserService
             return await response.Content.ReadFromJsonAsync<User>();
         return null;
     }
+
+    public async Task<List<User>> GetUsersByRoleAsync(string role)
+    {
+        return await _http.GetFromJsonAsync<List<User>>($"api/user/role/{role}") ?? new();
+    }
+
+    public async Task<List<User>> GetUsersByHotelAsync(string hotel)
+    {
+        return await _http.GetFromJsonAsync<List<User>>($"api/user/hotel/{hotel}") ?? new();
+    }
     
-    // Skal kigges igennem, måske kan den laves anderledes
     public async Task<User?> GetCurrentUserAsync()
     {
-        var userJson = await _js.InvokeAsync<string>("localStorage.getItem", "loggedInUser");
-
-        return string.IsNullOrEmpty(userJson)
-            ? null
-            : JsonSerializer.Deserialize<User>(userJson, new JsonSerializerOptions
-            {
-                PropertyNameCaseInsensitive = true
-            });
+        return await _localStorage.GetItemAsync<User>("loggedInUser");
     }
 
     public async Task<bool> DeleteUserAsync(int userId)
@@ -58,5 +64,4 @@ public class UserService : IUserService
         var response = await _http.PutAsJsonAsync($"api/user/{userId}/status", newStatus);
         return response.IsSuccessStatusCode;
     }
-
 }
